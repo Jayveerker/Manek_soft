93589c4fade28b17d2ac6e9fd1db214e419cc7a1
hanif321 <hanif.kerinfotech@gmail.com>
2024-03-04 15:43:28 +0530

    new commit

--- assets/js/conunt.js
+++ assets/js/conunt.js
@@ -8,7 +8,7 @@ let count4 = 1;
 let projectCount = setInterval(projectCounter, 1);
 function yearCounter() {
   count++;
-  document.querySelector("#number1").innerHTML = count + "Years";
+  document.querySelector("#number1").innerHTML = count + "Years+";
   if (count == 11) {
     clearInterval(yearCount);
   }
@@ -16,7 +16,7 @@ function yearCounter() {
 
 function memberCounter() {
   count2++;
-  document.querySelector("#number2").innerHTML = count2 + "";
+  document.querySelector("#number2").innerHTML = count2 + "+";
   if (count2 == 300) {
     clearInterval(memberCount);
   }
@@ -24,14 +24,14 @@ function memberCounter() {
 
 function clintCounter() {
   count3++;
-  document.querySelector("#number3").innerHTML = count3 + "";
+  document.querySelector("#number3").innerHTML = count3 + "+";
   if (count3 == 425) {
     clearInterval(clintCount);
   }
 }
 function projectCounter() {
   count4++;
-  document.querySelector("#number4").innerHTML = count4 + "";
+  document.querySelector("#number4").innerHTML = count4 + "+";
   if (count4 == 855) {
     clearInterval(projectCount);
   }
